4e7921cebae4a4b84f18c988e5abb6f2d8134d0c
supmit13 <supmit@gmail.com>
2014-11-27 01:29:18 +0530

    First commit containing speech to text PoC script

---
+++ speech2.py
@@ -0,0 +1,34 @@
+import sys,os
+
+def decodeSpeech(hmmd,lmdir,dictp,wavfile):
+    """
+    Decodes a speech file
+    """
+
+    try:
+        import pocketsphinx as ps
+        import sphinxbase
+
+    except:
+        print """Pocket sphinx and sphixbase is not installed
+        in your system. Please install it with package manager.
+        """
+
+    speechRec = ps.Decoder(hmm = hmmd, lm = lmdir, dict = dictp)
+    wavFile = file(wavfile,'rb')
+    wavFile.seek(44)
+    speechRec.decode_raw(wavFile)
+    result = speechRec.get_hyp()
+
+    return result[0]
+
+if __name__ == "__main__":
+    hmdir = "/usr/share/pocketsphinx/model/hmm/wsj1"
+    lmd = "/usr/share/pocketsphinx/model/lm/wsj/wlist5o.3e-7.vp.tg.lm.DMP"
+    dictd = "/usr/share/pocketsphinx/model/lm/wsj/wlist5o.dic"
+    wavfile = sys.argv[1]
+    recognised = decodeSpeech(hmdir,lmd,dictd,wavfile)
+
+    print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    print recognised
+    print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
