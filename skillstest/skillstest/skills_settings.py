90ce44a0247342289efbea2b3744c29cfd1390b3
supmit13 <supmit@gmail.com>
2014-12-12 01:55:39 +0530

    Created Django's project files hierarchical structure

---
+++ skillstest/skillstest/skills_settings.py
@@ -0,0 +1,9 @@
+import os, sys, re, time
+
+TEST_TOPICS = ("Programming", "Project Management", "Database Management", "Quality Assurance", \
+               "Software Testing", "Business Development", "Product Development", "Customer Service",\
+               "Software Architecture", "Delivery Management")
+
+TEST_TYPES = ("Multiple Choice", "Fill up the Blanks", "Subjective", "Coding", "Algorithm", "Composite")
+
+TEST_SEARCH_SCOPE = ("Public Tests Only", "Only Tests Accessible by the Recruiter", "Private Tests Only", "All Tests")
