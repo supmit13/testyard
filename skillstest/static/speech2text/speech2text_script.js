9fe4610b5c5cb2cc4a6c6139f8cd808378f9b55a
Supriyo Mitra <smitra@artinfo.com>
2018-09-22 04:24:32 +0530

    Adding speech to text functionality in formulating challenge/question statements

---
+++ skillstest/static/speech2text/speech2text_script.js
@@ -0,0 +1,106 @@
+try {
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+}
+catch(e) {
+  console.error(e);
+  $('.no-browser-support').show();
+  $('.app').hide();
+}
+
+
+var challengetextarea = $('#statement');
+var instructions = $('#recording-instructions');
+
+var challengecontent = '';
+
+// Get all notes from previous sessions and display them.
+var challengestatement = getchallenge();
+
+/*-----------------------------
+      Voice Recognition 
+------------------------------*/
+
+// If false, the recording will stop after a few seconds of silence.
+// When true, the silence period is longer (about 15 seconds),
+// allowing us to keep recording even when the user pauses. 
+recognition.continuous = true;
+
+// This block is called every time the Speech APi captures a line. 
+recognition.onresult = function(event) {
+
+  // event is a SpeechRecognitionEvent object.
+  // It holds all the lines we have captured so far. 
+  // We only need the current one.
+  var current = event.resultIndex;
+
+  // Get a transcript of what was said.
+  var transcript = event.results[current][0].transcript;
+
+  // Add the current transcript to the contents of our Note.
+  // There is a weird bug on mobile, where everything is repeated twice.
+  // There is no official solution so far so we have to handle an edge case.
+  var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+
+  if(!mobileRepeatBug) {
+    challengecontent += transcript;
+    challengetextarea.val(challengecontent);
+  }
+};
+
+recognition.onstart = function() { 
+  instructions.text('Voice recognition activated. Try speaking into the microphone.');
+}
+
+recognition.onspeechend = function() {
+  instructions.text('You were quiet for a while so voice recognition turned itself off.');
+}
+
+recognition.onerror = function(event) {
+  if(event.error == 'no-speech') {
+    instructions.text('No speech was detected. Try again.');  
+  };
+}
+
+
+
+/*-----------------------------
+      App buttons and input 
+------------------------------*/
+
+$('#start-record-btn').on('click', function(e) {
+  if (challengecontent.length) {
+    challengecontent += ' ';
+  }
+  recognition.start();
+});
+
+
+$('#pause-record-btn').on('click', function(e) {
+  recognition.stop();
+  instructions.text('Voice recognition paused.');
+});
+
+// Sync the text inside the text area with the challengecontent variable.
+challengetextarea.on('input', function() {
+  challengecontent = $(this).val();
+})
+
+$('#save-challenge-btn').on('click', function(e) {
+  recognition.stop();
+
+  if(!challengecontent.length) {
+    instructions.text('Could not save empty challenge statement. Please add some challenge/question to your test.');
+  }
+      
+})
+
+
+/*-----------------------------
+      Helper Functions 
+------------------------------*/
+
+function getchallenge() {
+  return ${challengetextarea.content};
+}
+
