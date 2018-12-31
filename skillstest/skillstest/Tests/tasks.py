4a5ae346162e8aafef54dfc3414f78dc196e6958
Supriyo Mitra <smitra@artinfo.com>
2018-12-31 21:33:54 +0530

    Rectified tasks.py to handle math expressions with hex symbols

--- skillstest/skillstest/Tests/tasks.py
+++ skillstest/skillstest/Tests/tasks.py
@@ -11,6 +11,20 @@ import urllib,urllib2
 from BeautifulSoup import BeautifulSoup
 import shutil
 
+hex_to_ascii = {'%20' : ' ', '%21' : '!', '%22' : '"', '%23' : '#', '%24' : '$', '%25' : '%', '%26' : '&', \
+          '%27' : "'", '%28' : '(', '%29' : ')', '%2A' : '*', '%2B' : '+', '%2C' : ',', '%2D' : '-', '%2E' : '.', '%2F' : '/', '%30' : '0',\
+	  '%31' : '1', '%32' : '2', '%33' : '3', '%34' : '4', '%35' : '5', '%36' : '6', '%37' : '7', '%38' : '8', '%39' : '9', '%3A' : ':',\
+	  '%3B' : ';', '%3C' : '<', '%3D' : '=', '%3E' : '>', '%3F' : '?', '%40' : '@', '%41' : 'A', '%42' : 'B', '%43' : 'C', '%44' : 'D',\
+	  '%45' : 'E', '%46' : 'F', '%47' : 'G', '%48' : 'H', '%49' : 'I', '%4A' :  'J', '%4B' : 'K', '%4C' : 'L', '%4D' : 'M', '%4E' : 'N',\
+	  '%4F' : 'O', '%50' : 'P', '%51' : 'Q', '%52' : 'R', '%53' : 'S', '%54' : 'T', '%55' : 'U', '%56' : 'V', '%57' : 'W', '%58' : 'X',\
+	  '%59' : 'Y', '%5A' : 'Z', '%5B' : '[', '%5C' : '\\', '%5D' : ']', '%5E' : '^', '%5F' : '_', '%60' : '`', '%61' : 'a', '%62' : 'b',\
+	  '%63' : 'c', '%64' : 'd', '%65' : 'e', '%66' : 'f', '%67' : 'g', '%68' : 'h', '%69' : 'i', '%6A' : 'j', '%6B' : 'k', '%6C' : 'l',\
+	  '%6D' : 'm', '%6E' : 'n', '%6F' : 'o', '%70' : 'p', '%71' : 'q', '%72' : 'r', '%73' : 's', '%74' : 't', '%75' : 'u', '%76' : 'v',\
+	  '%77' : 'w', '%78' : 'x', '%79' : 'y', '%7A' : 'z', '%7B' : '{', '%7C' : '|' , '%7D' : '}', '%7E' : '~' }
+
+
+
+
 """
 This scans all tests and activates the ones whose publish and 
 activate dates are in the past. It also sets the 'status' flag
@@ -142,8 +156,12 @@ def process_answer_scripts():
             try:
                 challengeobj = Challenge.objects.filter(test=testobj).filter(statement=challengestatement)[0]
             except:
-                print "Could not find the challenge '%s'"%challengestatement
-                continue 
+                for k in hex_to_ascii.keys():
+                    challengestatement = challengestatement.replace(k, hex_to_ascii[k])
+                challengeobj = Challenge.objects.filter(test=testobj).filter(statement=challengestatement)[0]
+                if not challengeobj:
+                    print "Could not find the challenge '%s'"%challengestatement
+                    continue 
             userrespobj.challenge = challengeobj
             inputdatatag, inputdata = None, ""
             # Now parse the resp variable to find the user's response
