4a6343a7065d627e45ee2d1c4786fef04f88d14c
Supriyo Mitra <smitra@artinfo.com>
2019-02-04 23:39:40 +0530

    Made some changes in mathjax processing. However, the evaluation screen issue with LaTeX expressions still exists in this commit. Hoping to fix it in the next commit

--- skillstest/static/javascript/mathjaxjs.js
+++ skillstest/static/javascript/mathjaxjs.js
@@ -1,23 +1,34 @@
 
-	  MathJax.Hub.Config({ 
-	    extensions: ['tex2jax.js'], 
-	    jax: ['input/TeX','output/HTML-CSS'], 
-	    tex2jax: {inlineMath: [['\[','\]'],['\\(','\\)']]} 
-	  }); 
-	</script> 
-        <script src='static/mathlib/math.min.js'></script> 
-        <script type='text/javascript' async src='static/MathJax/MathJax.js?config=TeX-MML-AM_CHTML'> 
-        </script> 
-        <script> 
- 
-        function displayMathExprEvalRespView(){ 
-            //alert("HELLO"); 
-	    mathexprcndtrespdiv = document.getElementById('mathexprcdusvwcndtresp'); 
-	    txtContents = mathexprcndtrespdiv.innerHTML; 
-	    MathJax.Hub.Queue(['Typeset', MathJax.Hub, mathexprcndtrespdiv]); 
- 
-            mathexprcndtchlngdiv = document.getElementById('mathexprcdusvwcndtch'); 
-	    txtContents = mathexprcndtchlngdiv.innerHTML;  
-	    MathJax.Hub.Queue(['Typeset', MathJax.Hub, mathexprcndtchlngdiv]); 
+	
+        function displayMathExprEvalRespView(countresp){ 
+	    MathJax.Hub.Config({
+	        extensions: ["tex2jax.js"],
+	        jax: ["input/TeX","output/HTML-CSS"],
+		showProcessingMessages: true,
+	        tex2jax: {inlineMath: [['$','$'], ["\[","\]"],["\\(","\\)"]]}
+	    });
+
+            for(var i=1; i < countresp; i++){
+	        var mathexprcndtrespdiv = document.getElementById("mathexprcdusvwcndtresp_" + i);
+	        var mathrespcontent = mathexprcndtrespdiv.innerHTML;
+	        mathrespcontent = mathrespcontent.replace(/\\?\[\s*/, "");
+	        mathrespcontent = mathrespcontent.replace(/\\?\]\s*/, "");
+	        mathexprcndtrespdiv.innerHTML = mathrespcontent;
+	        //alert(mathexprcndtrespdiv.innerHTML);
+	        var mathexprcndtchlngdiv = document.getElementById("mathexprcdusvwcndtch_" + i);
+	        var mathexprcontent = mathexprcndtchlngdiv.innerHTML;
+	        mathexprcontent = mathexprcontent.replace(/\\?\[\s*/, "");
+	        mathexprcontent = mathexprcontent.replace(/\\?\]\s*/, "");
+	        mathexprcndtchlngdiv.innerHTML = mathexprcontent;
+	        try{
+		    MathJax.Hub.Queue(['Typeset', MathJax.Hub, mathexprcndtchlngdiv]);
+		    MathJax.Hub.Queue(['Typeset', MathJax.Hub, mathexprcndtrespdiv]);
+	        }
+	        catch(err){
+		    alert(err.message);
+	        }
+            }
+	    //alert("Number = " + countresp);
+	    //alert(MathJax.Hub.Queue);
 	} 
  
