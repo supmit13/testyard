fcb8cc549a085e0e6ea7bbbfaf1cc8748fc4e874
Supriyo Mitra <smitra@artinfo.com>
2019-02-25 19:52:20 +0530

    Made minor changes while trying to fix the mathjax issue in the evaluation screen (which is not resolved as yet - getting a bit of a pain in the ass).

--- skillstest/static/javascript/mathjaxjs.js
+++ skillstest/static/javascript/mathjaxjs.js
@@ -1,34 +1,28 @@
+// Javascript for displaying mathjax symbols in questions and answers on evaluation page. -- Supriyo.
 
-	
-        function displayMathExprEvalRespView(countresp){ 
-	    MathJax.Hub.Config({
-	        extensions: ["tex2jax.js"],
-	        jax: ["input/TeX","output/HTML-CSS"],
-		showProcessingMessages: true,
-	        tex2jax: {inlineMath: [['$','$'], ["\[","\]"],["\\(","\\)"]]}
-	    });
+function displayMathExprEvalRespView(countresp){ 
+    //alert(countresp);
+    //alert(typeof MathJax.Hub); // Works! Prints 'object'.
+    for(i=1; i < countresp; i++){
+        mathexprcndtrespdiv = document.getElementById("mathexprcdusvwcndtresp_" + i.toString());
+        mathrespcontent = mathexprcndtrespdiv.innerHTML;
+        mathrespcontent = mathrespcontent.replace(/^\\?\[\s*/, "");
+        mathrespcontent = mathrespcontent.replace(/\\?\]\s*$/, "");
+        mathexprcndtrespdiv.innerHTML = mathrespcontent;
+        //alert(mathexprcndtrespdiv.innerHTML);
+	MathJax.Hub.Queue(["Typeset", MathJax.Hub, mathexprcndtrespdiv]);
+	//alert("mathexprcdusvwcndtch_" + i.toString());
+        mathexprcndtchlngdiv = document.getElementById("mathexprcdusvwcndtch_" + i.toString());
+	//mathexprcndtchlngdiv = document.getElementById("challengestatement");
+        mathexprcontent = mathexprcndtchlngdiv.innerHTML;
+        mathexprcontent = mathexprcontent.replace(/\\?\[\s*/, "");
+        mathexprcontent = mathexprcontent.replace(/\\?\]\s*/, "");
+        mathexprcndtchlngdiv.innerHTML = mathexprcontent;
+        //alert(mathexprcndtchlngdiv.innerHTML);
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, mathexprcndtchlngdiv]);
+        //MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    }
+    //alert("Number = " + countresp);
+    //alert(MathJax.Hub.Queue.length); // Prints 0.
+}
 
-            for(var i=1; i < countresp; i++){
-	        var mathexprcndtrespdiv = document.getElementById("mathexprcdusvwcndtresp_" + i);
-	        var mathrespcontent = mathexprcndtrespdiv.innerHTML;
-	        mathrespcontent = mathrespcontent.replace(/\\?\[\s*/, "");
-	        mathrespcontent = mathrespcontent.replace(/\\?\]\s*/, "");
-	        mathexprcndtrespdiv.innerHTML = mathrespcontent;
-	        //alert(mathexprcndtrespdiv.innerHTML);
-	        var mathexprcndtchlngdiv = document.getElementById("mathexprcdusvwcndtch_" + i);
-	        var mathexprcontent = mathexprcndtchlngdiv.innerHTML;
-	        mathexprcontent = mathexprcontent.replace(/\\?\[\s*/, "");
-	        mathexprcontent = mathexprcontent.replace(/\\?\]\s*/, "");
-	        mathexprcndtchlngdiv.innerHTML = mathexprcontent;
-	        try{
-		    MathJax.Hub.Queue(['Typeset', MathJax.Hub, mathexprcndtchlngdiv]);
-		    MathJax.Hub.Queue(['Typeset', MathJax.Hub, mathexprcndtrespdiv]);
-	        }
-	        catch(err){
-		    alert(err.message);
-	        }
-            }
-	    //alert("Number = " + countresp);
-	    //alert(MathJax.Hub.Queue);
-	} 
- 
