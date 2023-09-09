edad501d61222e81c0dca018d57e6f49c60217f3
supmit13 <supmit@gmail.com>
2023-09-10 03:42:30 +0530

    Minor bug fix

--- testyard/skillstest/skillstest/Tests/views.py
+++ testyard/skillstest/skillstest/Tests/views.py
@@ -2854,6 +2854,7 @@ def editexistingtest(request):
     create_test_dict['attemptsintervalunit'] = testobj.attemptsintervalunit
     create_test_dict['exist_test_id'] = testid
     create_test_dict['numchallenges'] = testobj.challengecount
+    create_test_dict['passscore'] = testobj.passscore
     create_test_dict['createtesturl'] = skillutils.gethosturl(request) + '/' + mysettings.CREATE_TEST_URL
     create_test_dict['evalgroupslitags'] = evalgroupslitags
     #create_test_dict['datepickercode'] = "<link rel='stylesheet' type='text/css' media='all' href='static/datepick/jsDatePick_ltr.min.css' /><script type='text/javascript' src='static/datepick/jsDatePick.min.1.3.js'></script><script>new JsDatePick({ useMode:2, target:'publishdate', dateFormat:'%d-%M-%Y', limitToToday : false });  new JsDatePick({ useMode:2, target:'activedate', dateFormat:'%d-%M-%Y', limitToToday : false });</script>"
