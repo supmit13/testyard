5896ee825b15368cfdcb2fb60b45b4d8c4c4aaf3
supmit13 <supmit@gmail.com>
2024-04-03 01:29:54 +0530

    Changes due to dev of captcha service

--- testyard/skillstest/skillstest/utils.py
+++ testyard/skillstest/skillstest/utils.py
@@ -1456,11 +1456,11 @@ def generate_captcha(w=280, h=90):
     captchatext = generate_random_text(1) # We need one word only.
     captchakey = generate_random_string()
     # Get imagedata from service 'urlshortener' url '/captcha'
-    httpheaders = { 'User-Agent' : r'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.10) Gecko/20111103 Firefox/3.6.24',  'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language' : 'en-us,en;q=0.5', 'Accept-Encoding' : 'gzip,deflate', 'Accept-Charset' : 'ISO-8859-1,utf-8;q=0.7,*;q=0.7', 'Keep-Alive' : '115', 'Connection' : 'keep-alive', 'content-type' : 'application/json'}
+    httpheaders = { 'User-Agent' : r'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.10) Gecko/20111103 Firefox/3.6.24',  'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language' : 'en-us,en;q=0.5', 'Accept-Encoding' : 'gzip,deflate', 'Accept-Charset' : 'ISO-8859-1,utf-8;q=0.7,*;q=0.7', 'Keep-Alive' : '115', 'Connection' : 'keep-alive'}
     requesturl = mysettings.URL_SHORTENER_HOST + "/captcha"
     requestdict = { 'captchatext' : captchatext}
     requestdata = json.dumps(requestdict).encode("utf-8")
-    httpheaders['content-length'] = requestdata.__len__()
+    httpheaders['Content-Length'] = requestdata.__len__()
     requestobj = urllib2.Request(requesturl, data=requestdata, headers=httpheaders)
     opener = createopener()
     responseobj = None
@@ -1469,8 +1469,7 @@ def generate_captcha(w=280, h=90):
     except:
         print("Error: %s"%sys.exc_info()[1].__str__())
         return ("", captchakey)
-    #imagedata = responseobj.read()
-    imagedata = "captcha"
+    imagedata = responseobj.read()
     captcha = Captcha()
     captcha.captchatext = captchatext
     captcha.captchakey = captchakey
